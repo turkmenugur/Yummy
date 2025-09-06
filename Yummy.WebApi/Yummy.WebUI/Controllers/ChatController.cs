d670c971f333a9ecc45f1d3ec8604d3e8289979b
Uğur Türkmen <trkmn.ugur.12@gmail.com>
2025-09-07 01:28:20 +0300

    feat(dashboard): rezervasyon metriklerini HTTP’ye al ve Revenue Chart VC ekle

---
+++ Yummy.WebApi/Yummy.WebUI/Controllers/ChatController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Yummy.WebUI.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: ChatController
+        public ActionResult SendChatWithAI()
+        {
+            return View();
+        }
+
+    }
+}
