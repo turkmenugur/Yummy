db8caa4f35fe1315c549911df5533942fdfd8b4e
Uğur Türkmen <trkmn.ugur.12@gmail.com>
2025-09-19 12:46:46 +0300

    Dashboard'da rezervasyon ve müşteri verilerinin dinamik çekilmesi, onaylanan rezervasyon sayısı sorgusunun düzeltilmesi ve rezervasyon durum grafiği eklendi

---
+++ Yummy.WebApi/Yummy.WebApi/Dtos/ReservationDtos/ReservationChartDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Yummy.WebApi.Dtos.ReservationDtos
+{
+    public class ReservationChartDto
+    {
+        public string Month { get; set; }
+        public int Approved { get; set; }
+        public int Pending { get; set; }
+        public int Canceled { get; set; }
+    }
+}
--- Yummy.WebApi/Yummy.WebUI/Models/RevenueChartViewModels.cs
+++ Yummy.WebApi/Yummy.WebUI/Models/RevenueChartViewModels.cs
@@ -2,7 +2,16 @@
 
 public class RevenueChartViewModel
 {
-    public List<string> Labels { get; set; } = new();
+    public List<String> Labels { get; set; } = new();
+    public List<int> Income { get; set; } = new();
+    public List<int> Expense { get; set; } = new();
+    public int TotalReservations { get; set; }
+    public int ApprovedReservations { get; set; }
+    public int CanceledReservations { get; set; }
+}
+
+/*
+ * public List<string> Labels { get; set; } = new();
     public List<int> Income { get; set; } = new();
     public List<int> Expense { get; set; } = new();
 
@@ -16,4 +25,4 @@ public class RevenueChartViewModel
     public int ProjectCompleted { get; set; }
     public decimal TotalExpense { get; set; }
     public int NewCustomers { get; set; }
-}
+ */
